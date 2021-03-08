7b1857b881789ec004e4c8a022d47b45c19cd8e8
Diana Buffone <diana.pamelaet@gmail.com>
2021-03-07 20:16:24 -0500

    Complete api call user story 2 of 9

---
+++ app/services/nager_date_service.rb
@@ -0,0 +1,10 @@
+class NagerDateService < ApiService
+  def self.get_holiday_dates
+    holidays = "https://date.nager.at/Api/v2/NextPublicHolidays/us"
+    data = get_data(holidays)
+    data = data[0..2]
+    holidays = data.map do |date|
+      Holiday.new(date)
+    end
+  end
+end
