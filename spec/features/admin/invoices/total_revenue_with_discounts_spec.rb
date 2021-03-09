8dc3384e6d9b569c4988ea68185efc2079c4d4a5
Diana Buffone <diana.pamelaet@gmail.com>
2021-03-09 16:58:26 -0500

    Add partial test for user story 9 of 9

---
+++ spec/features/admin/invoices/total_revenue_with_discounts_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe 'Admin Invoices Index Page' do
+  before :each do
+    @m1 = Merchant.create!(name: 'Merchant 1')
+
+    @c1 = Customer.create!(first_name: 'Yo', last_name: 'Yoz', address: '123 Heyyo', city: 'Whoville', state: 'CO', zip: 12345)
+    @c2 = Customer.create!(first_name: 'Hey', last_name: 'Heyz')
+
+    @i1 = Invoice.create!(customer_id: @c1.id, status: 2, created_at: '2012-03-25 09:54:09')
+    @i2 = Invoice.create!(customer_id: @c2.id, status: 1, created_at: '2012-03-25 09:30:09')
+
+    @item_1 = Item.create!(name: 'test', description: 'lalala', unit_price: 6, merchant_id: @m1.id)
+    @item_2 = Item.create!(name: 'rest', description: 'dont test me', unit_price: 12, merchant_id: @m1.id)
+
+    @ii_1 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_1.id, quantity: 12, unit_price: 2, status: 0)
+    @ii_2 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_2.id, quantity: 6, unit_price: 1, status: 1)
+    @ii_3 = InvoiceItem.create!(invoice_id: @i2.id, item_id: @item_2.id, quantity: 87, unit_price: 12, status: 2)
+
+    visit admin_invoice_path(@i1)
+  end
+
+  it 'should display the total revenue the invoice will generate' do
+    expect(page).to have_content(@i1.total_revenue_with_discount)
+
+    expect(page).to_not have_content(@i2.total_revenue_with_discount)
+  end
+end
