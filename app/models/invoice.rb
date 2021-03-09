11a24cbf843cd8d69302265739df6fd118da5b9e
Diana Buffone <diana.pamelaet@gmail.com>
2021-03-08 23:00:11 -0500

    Add relationships and link back to dashboard

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -7,10 +7,16 @@ class Invoice < ApplicationRecord
   has_many :invoice_items
   has_many :items, through: :invoice_items
   has_many :merchants, through: :items
+  has_many :bulk_discounts, through: :merchants
 
   enum status: [:cancelled, :in_progress, :complete]
 
   def total_revenue
     invoice_items.sum("unit_price * quantity")
   end
+
+  def total_bulk_discount
+    # invoice_items.joins(:bulk_discounts)
+  end
 end
+# Then I see that the total revenue for my merchant includes bulk discounts in the calculation
